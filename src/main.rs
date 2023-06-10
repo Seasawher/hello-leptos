20acf053d92fd4492bd2522b73a70001f94679f3
Seasawher <tellagoodstory@icloud.com>
2023-06-11 02:57:35 +0900

    プログレスバーを設置

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,22 @@ fn App(cx: Scope) -> impl IntoView {
             on:click=move |_| {
                 set_count.update(|n| *n += 1);
             }
+            class:red=move || count() % 2 == 1
         >
         "Click me: "
         {move || count.get()}
         </button>
+
+        <ProgressBar progress=count />
+    }
+}
+
+#[component]
+fn ProgressBar(cx: Scope, progress: ReadSignal<i32>) -> impl IntoView {
+    view! {cx,
+        <progress
+            max="50"
+            value=progress
+        />
     }
 }
